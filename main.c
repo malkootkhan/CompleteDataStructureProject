d05489970f35646163acae735062ff7640867a5b
khan <khan@ubuntu.myguest.virtualbox.org>
2023-11-18 18:47:20 +0000

    properly implemented the queue in main.c

--- main.c
+++ main.c
@@ -2,15 +2,36 @@
 
 
 int main(){
-    int cap;
+    int cap, choice,d;
     struct myQueue *q=NULL;
     printf("Enter size of queue: ");
     scanf("%d", &cap);
     q = createQueue(cap);
-    enQueue(q, 12);
-    enQueue(q, 13);
-    enQueue(q, 14);
-    enQueue(q, 15);
-    displayQueue(q);
+    while (TRUE){
+        printf("press '1' for enqueue/insert data\n");
+        printf("press '2' for dequeue/delete data\n");
+        printf("press '3' for display data\n");
+        printf("press '4' for exit the program\n");
+        printf("\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch(choice){
+            case 1:
+                printf("Enter data: ");
+                scanf("%d",&d);
+                enQueue(q,d);
+                break;
+            case 2:
+                deQueue(q);
+                break;
+            case 3:
+                displayQueue(q);
+                break;
+            case 4:
+                exit(0);
+
+        }
+    }
     return 0;
+
 }
